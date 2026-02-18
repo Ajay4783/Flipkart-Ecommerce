6077fc344d335fd7f5eb1a184d52e6cdfb718d35
Ajay4783 <ajay.me4783@gmail.com>
2026-02-18 22:00:40 +0530

    Fixed email port to 465 SSL

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -135,8 +135,8 @@ LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 467
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
