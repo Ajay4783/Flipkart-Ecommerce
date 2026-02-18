4f391781e7dc20cb5dad2403fcd766be350027e7
Ajay4783 <ajay.me4783@gmail.com>
2026-02-18 19:02:34 +0530

    Redirected home and changed admin URL

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('blog/', permanent=True)),
+    path('secret-ajay-portal/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
 if settings.DEBUG:
