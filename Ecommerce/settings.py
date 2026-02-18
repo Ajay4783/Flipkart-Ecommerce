a2ac2baf48a2c8ec14048d293e7da6e61e667ccc
Ajay4783 <ajay.me4783@gmail.com>
2026-02-18 16:24:00 +0530

    Fixed STATIC_ROOT setting

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Optional but recommended for development
 STATICFILES_DIRS = [
